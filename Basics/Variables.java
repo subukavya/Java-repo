4bec5f09547d3a6fe356104380bba5ec14a35b8c
subukavya <kavyasubu1207@gmail.com>
2026-01-27 11:40:56 +0530

    Refactor variable declarations and initializations

--- Basics/Variables.java
+++ Basics/Variables.java
@@ -1,34 +1,26 @@
 class Variables {
     public static void main(String[] args) {
 
-        // Integer types
-        byte b = 10;
-        short s = 200;
-        int i = 5000;
-        long l = 100000L;
+        // Variable declaration
+        int age;
+        float marks;
+        char grade;
+        boolean isPassed;
 
-        // Floating point types
-        float f = 10.5f;
-        double d = 99.99;
+        // Variable initialization
+        age = 20;
+        marks = 85.5f;
+        grade = 'A';
+        isPassed = true;
 
-        // Character type
-        char c = 'A';
+        // Printing variables
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + isPassed);
 
-        // Boolean type
-        boolean isJavaFun = true;
-
-        // String (Non-primitive)
-        String name = "Java Learner";
-
-        // Output
-        System.out.println("Byte value: " + b);
-        System.out.println("Short value: " + s);
-        System.out.println("Int value: " + i);
-        System.out.println("Long value: " + l);
-        System.out.println("Float value: " + f);
-        System.out.println("Double value: " + d);
-        System.out.println("Char value: " + c);
-        System.out.println("Boolean value: " + isJavaFun);
+        // Declaration + initialization together
+        String name = "Java Student";
         System.out.println("Name: " + name);
     }
 }
