28bae4f0422d5c0225b74a6d47284ba4c59b78bc
Kavya N <kavyasubu1207@gmail.com>
2026-02-21 09:21:24 +0530

    Override equals method in Person class
    Implemented equals method to compare Person objects by id.

--- ObjectClassMethods/2_EqualsDemo.java
+++ ObjectClassMethods/2_EqualsDemo.java
@@ -1,1 +1,27 @@
 
+/*
+ * equals(Object obj)
+ * ------------------
+ * Used to compare two objects.
+ * Default: compares memory references.
+ * Override it to compare object data (content comparison).
+ */
+
+class Person {
+    int id;
+
+    Person(int id) {
+        this.id = id;
+    }
+
+    public boolean equals(Object obj) {
+        Person p = (Person) obj;
+        return this.id == p.id;
+    }
+
+    public static void main(String[] args) {
+        Person p1 = new Person(10);
+        Person p2 = new Person(10);
+        System.out.println(p1.equals(p2)); // true
+    }
+}
